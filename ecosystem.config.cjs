9a8fab256fe49a0a223276692e99b109320efc5b
WSL detailcore <qwe@Honor15>
2023-12-09 12:14:44 +0400

    pm2

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,6 +1,8 @@
 module.exports = {
-  name: 'converter',
+  name: 'pdf-2-img',
   script: './backend/index.js',
+  exec_mode: 'cluster',
+  instances: 2,
   max_memory_restart: '1500M',
   exp_backoff_restart_delay: 10000, // перезапуск с экспоненциальной отсрочкой будет постепенно увеличивать время между перезапусками, уменьшая нагрузку на вашу БД
 }
